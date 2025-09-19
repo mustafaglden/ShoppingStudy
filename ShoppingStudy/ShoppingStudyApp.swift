51cece7e0800519710750ce4405449173c4963f6
mustafagulden <mustafaagulden@gmail.com>
2025-09-19 11:38:57 +0300

    Initial Commit

---
+++ ShoppingStudy/ShoppingStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingStudyApp.swift
+//  ShoppingStudy
+//
+//  Created by Mustafa Gülden on 19.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
